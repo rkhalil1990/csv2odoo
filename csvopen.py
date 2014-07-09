7a8f9eb642780d5460f7b2dd36cdf549eb12603b
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-07-09 10:51:29 -0430

    [IMP] method gets type field in orm, in get_xml_id method is added when field is many2one and information come with search;field

--- csvopen.py
+++ csvopen.py
@@ -44,6 +44,59 @@ def print_values(config, opts):
         else:
             print "%s option has default value: %s" % (opt, option.default)
 
+def get_field_type(field, model_obj):
+    return model_obj.__dict__['_browse_class'].__dict__['__osv__'].get('columns').get(field).__dict__.get('type')
+
+def get_xml_id(field, tipo, csv_type, value, model_obj, ir_model_data_obj, oerp):
+
+    xml_id_str = False
+    if tipo == 'many2one':
+        #pdb.set_trace()
+        csv_type = csv_type.split(';')
+        if csv_type[0] == 'ref':
+            return value
+        elif csv_type[0] == 'search':
+            #pdb.set_trace()
+            field_search = csv_type[1]
+            relation_model_str = model_obj.__dict__['_browse_class'].__dict__['__osv__'].get('columns').get(field).__dict__.get('relation')
+            print 'campo: ' + field + ', tipo: ' + tipo + ', relation: ' + relation_model_str
+            relation_model_obj = oerp.get(relation_model_str)
+            field_id = relation_model_obj.search([(field_search,'=',value)])[0]
+
+            #_get_xml_id openerp/osv/orm.py +1125
+            xml_id = ir_model_data_obj.search([('model', '=', relation_model_str), ('res_id',
+                '=', field_id)])
+            if len(xml_id):
+                d = ir_model_data_obj.read(xml_id, ['name', 'module'])[0]
+                if d['module']:
+                    xml_id_str = '%s.%s' % (d['module'], d['name'])
+                else:
+                    xml_id_str = d['name']
+
+            else:
+                postfix = 0
+                while True:
+                    n = relation_model_str+'_'+str(field_id) + (postfix and ('_'+str(postfix)) or '' )
+                    if not ir_model_data_obj.search([('name', '=', n)]):
+                        break
+                    postfix += 1
+                ir_model_data_obj.create({
+                    'name': n,
+                    'model': relation_model_str,
+                    'res_id': field_id,
+                    'module': '__export__',
+                })
+                xml_id_str = '__export__.'+n
+            #_get_xml_id END
+
+            return xml_id_str
+    elif tipo == 'char':
+        return value
+    elif tipo == 'date':
+        #Revisar correcta sintaxis del date
+        print value
+
+
 def read_csv(csv_files, oerp):
     for csv_name in csv_files: 
         lines = csv.reader(open(csv_name))
@@ -60,21 +113,26 @@ def read_csv(csv_files, oerp):
             model_str = line.pop(1)
             model_obj = oerp.get(model_str)
 
-            pdb.set_trace()
             datas.append(line)
 
-        for field in field_names:
-            field = field.split(':')[0]
-            if field != 'id' and field != 'model':
-                tipo = model_obj.__dict__['_browse_class'].__dict__['__osv__'].get('columns').get(field).__dict__.get('type')
-                
-                if tipo == 'many2one':
-                    relation = model_obj.__dict__['_browse_class'].__dict__['__osv__'].get('columns').get(field).__dict__.get('relation')
-                    print 'campo: ' + field + ', tipo: ' + tipo + ', relation: ' + relation
-                else:
-                    print 'campo: ' + field + ', tipo: ' + tipo
+        for data in datas:
+            for i in xrange(0, len(field_names)):
+                #Preguntar aqui de que modo viene la informacion para poder buscar el xml_id que
+                #le corresponde
+                if data[i]:
+                    field = field_names[i]
+                    field = field.split(':')[0]
+                    if field not in ('id', 'model'):
+                        tipo = get_field_type(field, model_obj)
+                       
+                        xml_id = get_xml_id(field, tipo, fields_type[i], data[i], model_obj,
+                                ir_model_data_obj, oerp)
+                        data[i] = xml_id
 
-        print model_obj.import_data(field_names, datas, mode='init')
+
+        #pdb.set_trace()
+        print datas
+        print model_obj.import_data(field_names, datas, mode='init', current_module='__export__')
 
 def main(config, opts):
     #print_values(config, opts)
